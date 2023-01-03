6df82434ffda960e9308691048e0b1bf82b9836b
myaschmitz <48460138+myaschmitz@users.noreply.github.com>
2023-01-03 16:30:53 -0700

    Quick update on coordinates for map

--- climbing-point/src/components/Mapbox.js
+++ climbing-point/src/components/Mapbox.js
@@ -1,15 +1,15 @@
 import React, { useRef, useEffect, useState } from 'react';
 import "../App.js";
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import ReactMapGL, { Marker } from 'react-mapbox-gl';
+// import ReactMapGL, { Marker } from 'react-mapbox-gl';
 
 function Mapbox() {
     mapboxgl.accessToken = 'pk.eyJ1IjoibXlhc2NobWl0eiIsImEiOiJjbDlzMmtnbTAwN25nM3RzNTIxNDc4Z2NuIn0.WW5EcKyY2AQDi7m-513m4w';
 
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-122.33);
     const [lat, setLat] = useState(47.61);
+    const [lng, setLng] = useState(-122.33);
     const [zoom, setZoom] = useState(9);
 
     useEffect(() => {
@@ -26,8 +26,8 @@ function Mapbox() {
     useEffect(() => {
         if (!map.current) return; // wait for map to initialize
         map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
             setLat(map.current.getCenter().lat.toFixed(4));
+            setLng(map.current.getCenter().lng.toFixed(4));
             setZoom(map.current.getZoom().toFixed(2));
         });
     });
@@ -41,7 +41,7 @@ function Mapbox() {
     return (
         <div>
             <div className="sidebar">
-                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                Latitude: {lat} | Longitude: {lng} | Zoom: {zoom}
             </div>
             <div id="map" style={{ margin: 'auto' }} ref={mapContainer} className="map-container" />
         </div>
